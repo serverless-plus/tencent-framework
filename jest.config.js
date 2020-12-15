831236ab9e51ecd6e68c8742f37ebdd76a98505a
Yuga Sun <yuga_sun@163.com>
2020-12-15 17:19:04 +0800

    fix: update deploy and remove flow (#10)
    * fix: update deploy and remove flow
    
    * test: update

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,9 @@ require('dotenv').config({ path: join(__dirname, '.env.test') })
 const config = {
   verbose: true,
   silent: true,
-  testTimeout: 60000,
+  testTimeout: 600000,
   testEnvironment: 'node',
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
-  // testRegex: '(/__tests__/ci/ci.ssr\.(test|spec))\\.(js|ts)$',
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/__tests__/utils/', '/examples/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 }
